f86327c5b5f1e9e808fe32ad7dbbd33fc65b768f
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-09-13 17:18:34 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,5 +6,8 @@ variable "region" {}
 variable "ami" {}
 variable "instance_type" {}
 variable  "key_name" {} 
-
+variable "ec2_subnet" {}
+variable "ec2_security_groups" {}
+variable "elb_subnet" {}
+variable "elb_security_groups" {}
 
